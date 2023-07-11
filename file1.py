2c73bb8c682db65364747632383cc86d51912e55
Adri04p <138957375+Adri04p@users.noreply.github.com>
2023-07-10 19:00:49 -0500

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 10 18:58:43 2023
+
+@author: SYSTEMarket
+"""
+
+archivo=open("devices.txt")
+for elemento in archivo:
+    print(elemento)
+archivo.close()
