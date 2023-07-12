38adfd4770c62b41130671f2cd12f68073b9c979
Adri04p <138957375+Adri04p@users.noreply.github.com>
2023-07-12 08:52:13 -0500

    Add files via upload

---
+++ Actclase-math.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 12 08:51:48 2023
+
+@author: SYSTEMarket
+"""
+
+from math import ceil, floor, trunc
+
+x = 1.4
+y = 2.6
+
+print(floor(x), floor(y))
+print(floor(-x), floor(-y))
+
+
+from math import e, exp, log
+
+print(pow(e, 1) == exp(log(e)))
+print(pow2e, 2) == exp(2*log(2)))
+print(log(e, e) == exp(0))
---
+++ Actclase-numpy.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 12 08:51:14 2023
+
+@author: SYSTEMarket
+"""
+
+from numpy import *
+a1=array([1,2,3])
+a2=array([1,2,3,4])
+print(concatenate((a1,a2)))
