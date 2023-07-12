41073ec34dd69fe38d67eabc2492f5da6a2a95d4
Adri04p <138957375+Adri04p@users.noreply.github.com>
2023-07-11 22:26:39 -0500

    Add files via upload

---
+++ laboratorio.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 15 15:40:49 2019
+
+@author: UPS
+"""
+
+def readint(prompt, min, max):
+#
+# put your code here
+#
+    for i in range (-10,10):
+        i+=1
+v = readint("Enter a number from -10 to 10: ", -10, 10)
+
+print("The number is:", v)
+
+
+def readint(prompt, min, max):
+#
+# put your code here
+#
+    for i in range (-10,10):
+        i+=1
+v = readint("Enter a number from -10 to 10: ", -10, 10)
+
+print("The number is:", v)
+
